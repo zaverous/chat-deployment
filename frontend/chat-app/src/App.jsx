a139a47a0584e8a4f9ed9f4b5b83e8a1dbcfe3e3
lucia <lucia@Ubuntu-22.04.myguest.virtualbox.org>
2024-11-28 23:12:32 +0100

    login y leftsidebar

--- frontend/chat-app/src/App.jsx
+++ frontend/chat-app/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login/Login'
+import Chat from './pages/Chat/Chat'
+import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
 
 const App = () => {
   return (
--- frontend/chat-app/src/pages/Login/Login.jsx
+++ frontend/chat-app/src/pages/Login/Login.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Login.css'
+import assets from '../../assets/assets'
 
 const Login = () => {
+
+    const [currState,setCurrState] = useState("Sign up");
+
   return (
-    <div>
-      we are on the login page
+    <div className='login'>
+      <img src={assets.logo_big} alt="" className="logo" />
+      <form className="login-form">
+        <h2>{currState}</h2>
+        {currState === "Sign up"?<input type="text" placeholder='username' className="form-input" required/>:null}
+        <input type="email" placeholder='email address' className="form-input" required/>
+        <input type="password" placeholder='password' className="form-input" required/>
+        <button type="submit">{currState === "Sign up"?"Create account":"Login now"}</button>
+        <div className="login-term">
+          <input type="checkbox" />
+          <p>Agree to the terms of use & provacy policy</p>
+        </div>
+        <div className="login-forgot">
+          {
+            currState === "Sign up"
+            ?<p className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>Login here</span></p>
+            :<p className="login-toggle">Create an account <span onClick={()=>setCurrState("Sign up")}>click here</span></p>
+          }
+        </div>
+      </form>
     </div>
   )
 }

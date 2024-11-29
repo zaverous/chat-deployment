631beb63fdac2eb7829dc5e34f14a732d5319745
lucia <lucia@Ubuntu-22.04.myguest.virtualbox.org>
2024-11-29 12:19:05 +0100

    frontend basico

--- frontend/chat-app/src/components/ChatBox/ChatBox.jsx
+++ frontend/chat-app/src/components/ChatBox/ChatBox.jsx
@@ -1,10 +1,48 @@
 import React from "react";
 import './ChatBox.css'
+import assets from "../../assets/assets";
 
 const ChatBox = () => {
     return (
-        <div>
+        <div className="chat-box">
+            <div className="chat-user">
+                <img src={assets.profile_img} alt="" />
+                <p>Richard Sanford <img className="dot" src={assets.green_dot} alt="" /></p>
+                <img src={assets.help_icon} className="help" alt="" />
+            </div>
 
+            <div className="chat-msg">
+                <div className="s-msg">
+                    <p className="msg">Lorem ipsum is place holder text commonly used in ..</p>
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:30 PM</p>
+                    </div>
+                </div>
+                <div className="s-msg">
+                    <img className='msg-img' src={assets.pic1} alt="" />
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:30 PM</p>
+                    </div>
+                </div>
+                <div className="r-msg">
+                    <p className="msg">Lorem ipsum is place holder text commonly used in ..</p>
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:30 PM</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="chat-input">
+                <input type="text" placeholder="Send a message" />
+                <input type="text" id="image" placeholder="image/png, image/jpeg" hidden />
+                <label htmlFor="image">
+                    <img src={assets.gallery_icon} alt="" />
+                </label>
+                <img src={assets.send_button} alt="" />
+            </div>
         </div>
     )
 }
